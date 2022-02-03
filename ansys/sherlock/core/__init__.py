8be4c23efdbdaad69739619ed8044649cf683c5d
medson <medson@BLTMEDSON.win.ansys.com>
2022-02-03 14:31:16 -0500

    Added _version and launcher imports.

--- ansys/sherlock/core/__init__.py
+++ ansys/sherlock/core/__init__.py
@@ -1,3 +1,7 @@
 from ansys.sherlock.core.logging import Logger
+from ansys.sherlock.core._version import __version__
+from ansys.sherlock.core.launcher import (
+    launch_sherlock
+)
 
 LOG = Logger("sherlock")
