bbbd9c90f7d447277f93b131986bbf644a0392bd
medson <medson@BLTMEDSON.win.ansys.com>
2022-02-03 14:32:25 -0500

    Fixed path to launcher.

--- ansys/sherlock/core/__init__.py
+++ ansys/sherlock/core/__init__.py
@@ -1,6 +1,6 @@
 from ansys.sherlock.core.logging import Logger
 from ansys.sherlock.core._version import __version__
-from ansys.sherlock.core.launcher import (
+from ansys.sherlock.launcher import (
     launch_sherlock
 )
 
