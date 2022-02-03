be23c6d6d0e9a67450d2ff666f42b9bca8e812b6
medson <medson@BLTMEDSON.win.ansys.com>
2022-02-03 16:25:45 -0500

    Reordered imports.

--- ansys/sherlock/core/__init__.py
+++ ansys/sherlock/core/__init__.py
@@ -1,5 +1,5 @@
 from ansys.sherlock.core.logging import Logger
+LOG = Logger("sherlock")
+
 from ansys.sherlock.core._version import __version__
 from ansys.sherlock.launcher import launcher
-
-LOG = Logger("sherlock")
