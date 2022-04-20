dbd8f61541783b53a1c287e3fe74a80cd8c6b2a0
medson <medson@BLTMEDSON.win.ansys.com>
2022-04-20 10:49:38 -0400

    Removed unneeded settings.

--- setup.py
+++ setup.py
@@ -1,42 +1,11 @@
-"""Template setup file."""
-import codecs
-import os
-from io import open as io_open
-
 from setuptools import setup, find_packages
-
-# Use single source package versioning.  Follows:
-# https://packaging.python.org/guides/single-sourcing-package-version/
-#
-# With this approach, we can include the version within the setup file
-# while at the same time allowing the user to print the version from
-# the module
-HERE = os.path.abspath(os.path.dirname(__file__))
-__version__ = None
-version_file = os.path.join(HERE, 'ansys', 'product', 'library', '_version.py')
-with io_open(version_file, mode='r') as fd:
-    exec(fd.read())
-
-
-# Get the long description from the README file
-# This is needed for the description on PyPI
-def read(rel_path):
-    with codecs.open(os.path.join(HERE, rel_path), 'r') as fp:
-        return fp.read()
-
-
-with open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
-
 
 setup(
     name='ansys-sherlock',
-    version=__version__,
+    version='0.1.dev0',
     description='A Python wrapper for Ansys Sherlock gRPC APIs',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    #long_description=long_description,
-    #long_description_content_type='text/x-rst',
     url='https://github.com/pyansys/pySherlock/',
     license='MIT',
     author='ANSYS, Inc.',  # this is required
