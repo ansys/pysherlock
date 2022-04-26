789c4fefa9201a0350ba8b352c1d8598cdd33b78
Miu Lin Edson <miulin.edson@ansys.com>
2022-04-26 15:03:47 -0400

    Fixed pre-commit errors.
    Updated launcher.launch_sherlock to only accept port as a parameter.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,18 +3,18 @@ import sys
 
 from ansys.sherlock.core import __version__
 
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # Project information
-project = 'pysherlock'
-copyright = '(c) 2022 ANSYS, Inc. All rights reserved'
-author = 'ANSYS Inc.'
+project = "pysherlock"
+copyright = "(c) 2022 ANSYS, Inc. All rights reserved"
+author = "ANSYS Inc."
 release = version = __version__
 
 # optionally use the default pyansys logo
-html_logo = 'https://docs.pyansys.com/_static/pyansys-logo-black-cropped.png'
+html_logo = "https://docs.pyansys.com/_static/pyansys-logo-black-cropped.png"
 
-html_theme = 'pyansys_sphinx_theme'
+html_theme = "pyansys_sphinx_theme"
 
 # specify the location of your github repo
 html_theme_options = {
@@ -24,9 +24,9 @@ html_theme_options = {
 
 # Sphinx extensions
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
 ]
 
 autosummary_generate = True
@@ -35,13 +35,13 @@ autosummary_generate = True
 numpydoc_show_class_members = False  # we take care of autosummary on our own
 
 # The suffix(es) of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # static path
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # We have our own custom templates
-templates_path = ['_templates']
+templates_path = ["_templates"]
