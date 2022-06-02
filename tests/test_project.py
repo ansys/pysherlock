1342fea623301d90373257df6689935228764aac
Ricky Gao <ricky.gao@ansys.com>
2022-06-02 19:36:15 -0400

    Updated tests for importODB

--- tests/test_project.py
+++ tests/test_project.py
@@ -3,14 +3,25 @@ from ansys.sherlock import project
 
 def test_import_odb_archive():
     #   """Test delete_project API"""
-    #
-    #   Must evaluate results by looking at logging files, not automated
-    #   Refer to test_project.txt in tests
-    # TODO: Actually clean up these testcases
-    RC1, STR1 = project.import_odb_archive("")
-    assert RC1 == -1
-    assert STR1 == "Delete project error: Invalid Blank Project Name"
 
-    # RC2, STR2 = project.delete_project("test project")
-    # assert(RC2 == -1)
-    # assert(STR2 == "Delete project error: Cannot find project: {projectName} test project")
+    RC1, STR1 = project.import_odb_archive("hello", True, True, True, True)
+    assert RC1 == -1
+    assert STR1 == "Import ODB Error: Archive file name must end with [.tgz, .tar.gz, .tar]"
+
+    RC2, STR2 = project.import_odb_archive("hello.tgz", True, True, True, True)
+    assert RC2 == -1
+    assert STR2 == "Import ODB Error: Invalid ODB++ archive file path: hello.tgz"
+
+    RC3, STR3 = project.import_odb_archive(
+        "C:/Program Files/Ansys Inc/v231/sherlock/tutorial/ODB++ Tutorial.tgz",
+        True,
+        True,
+        True,
+        True,
+    )
+    assert RC3 == 0
+    assert STR3 == ""
+
+
+if __name__ == "__main__":
+    test_import_odb_archive()
