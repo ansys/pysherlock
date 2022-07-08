09884195fd087258508d723153c378b3f66bd21e
Ricky Gao <ricky.gao@ansys.com>
2022-07-08 14:16:31 -0400

    Fleshed out parameter checks for gen_stackup

--- src/ansys/sherlock/core/stackup.py
+++ src/ansys/sherlock/core/stackup.py
@@ -15,25 +15,59 @@ class Stackup(GrpcStub):
         """Initialize a gRPC stub for SherlockStackupService."""
         self.channel = channel
         self.stub = SherlockStackupService_pb2_grpc.SherlockStackupServiceStub(channel)
+        self.LAMINATE_THICKNESS_UNIT_LIST = None
+        self.LAMINATE_MATERIAL_MANUFACTURER_LIST = None
 
-    #     /**
-    #  * Represents the properties of a new stackup to be generated.
-    #  */
-    # message GenStackupRequest {
-    #   string project					= 1;	//
-    #   string ccaName					= 2;	// The CCA name.
-    #   double boardThickness				= 3;	// Board thickness.
-    #   string boardThicknessUnit 		= 4;	// Board thickness unit.
-    #   string pcbMaterialManufacturer	= 5;	// PCB material manufacturer.
-    #   string pcbMaterialGrade			= 6;	// PCB material grade.
-    #   string pcbMaterial				= 7;	// PCB material.
-    #   int32 conductorLayersCnt			= 8;	// Number of conductor layers.
-    #   double signalLayerThickness		= 9;	// Signal layer thickness.
-    #   string signalLayerThicknessUnit	= 10;	// Signal layer thickness unit.
-    #   double minLaminateThickness		= 11;	// Minimum laminate layer thickness.
-    #   string minLaminateThicknessUnit	= 12;	// Minimum laminate layer thickness unit.
-    #   bool maintainSymmetry				= 13;	// If set to true, maintain symmetry.
-    # }
+    def _init_laminate_thickness_units(self):
+        """Initialize LAMINATE_THICKNESS_UNIT_LIST."""
+        if self._is_connection_up():
+            laminate_thickness_unit_request = (
+                SherlockStackupService_pb2.ListLaminateThicknessUnitsRequest()
+            )
+            laminate_thickness_unit_response = self.stub.listLaminateThicknessUnits(
+                laminate_thickness_unit_request
+            )
+            if laminate_thickness_unit_response.returnCode.value == 0:
+                self.LAMINATE_THICKNESS_UNIT_LIST = laminate_thickness_unit_response.unit
+
+    def _init_laminate_material_manufacturers(self):
+        """Initialize LAMINATE_MATERIAL_MANUFACTURER_LIST."""
+        if self._is_connection_up():
+            laminate_material_manufacturer_request = (
+                SherlockStackupService_pb2.ListLaminateMaterialsManufacturersRequest()
+            )
+            laminate_material_manufacturer_response = self.stub.listLaminateMaterialsManufacturers(
+                laminate_material_manufacturer_request
+            )
+            if laminate_material_manufacturer_response.returnCode.value == 0:
+                self.LAMINATE_THICKNESS_UNIT_LIST = (
+                    laminate_material_manufacturer_response.manufacturer
+                )
+
+    def _check_pcb_material_validity(self, manufacturer, grade, material):
+        if (self.LAMINATE_MATERIAL_MANUFACTURER_LIST is not None) and (
+            manufacturer not in self.LAMINATE_MATERIAL_MANUFACTURER_LIST
+        ):
+            raise SherlockGenStackupError(
+                message="Invalid laminate manufacturer, grade, or material provided"
+            )
+        elif self._is_connection_up():
+            request = SherlockStackupService_pb2.ListLaminateMaterialsRequest(
+                manufacturer=manufacturer
+            )
+            response = self.stub.listLaminateMaterials(request)
+            if response.returnCode.value == 0:
+                manufacturer_materials = response.manufacturerMaterials[0]
+                assert manufacturer_materials.manufacturer == manufacturer
+
+                for grade_material in manufacturer_materials.gradeMaterials:
+                    if grade == grade_material.grade:
+                        if material in grade_material.laminateMaterial:
+                            return
+                        else:
+                            raise SherlockGenStackupError(
+                                "Invalid laminate manufacturer, grade, or material provided"
+                            )
 
     def gen_stackup(
         self,
@@ -83,8 +117,87 @@ class Stackup(GrpcStub):
             If set to true, maintain symmetry.
         Examples
         --------
-        TODO: Write example
+        >>> from ansys.sherlock.core.launcher import launch_sherlock
+        >>> sherlock = launch_sherlock()
+        >>> sherlock.project.import_odb_archive(
+            "ODB++ Tutorial.tgz",
+            True,
+            True,
+            True,
+            True,
+            project="Test",
+            cca_name="Card",
+        )
+        >>> sherlock.stackup.gen_stackup(
+            "Test",
+            "Card",
+            82.6,
+            "mil",
+            "Generic",
+            "FR-4",
+            "Generic FR-4",
+            6,
+            0.5,
+            "oz",
+            1.0,
+            "mil",
+            False,
+        )
         """
+        if self.LAMINATE_THICKNESS_UNIT_LIST is None:
+            self._init_laminate_thickness_units()
+        if self.LAMINATE_MATERIAL_MANUFACTURER_LIST is None:
+            self._init_laminate_material_manufacturers()
+
+        try:
+            if project == "":
+                raise SherlockGenStackupError(message="Invalid project name")
+            elif cca_name == "":
+                raise SherlockGenStackupError(message="Invalid cca name")
+            elif board_thickness < 0:
+                raise SherlockGenStackupError(message="Invalid board thickness provided")
+            elif board_thickness > 0:
+                if (
+                    self.LAMINATE_THICKNESS_UNIT_LIST is not None
+                ) and board_thickness_unit not in self.LAMINATE_THICKNESS_UNIT_LIST:
+                    raise SherlockGenStackupError(message="Invalid board thickness unit provided")
+            elif (self.LAMINATE_MATERIAL_MANUFACTURER_LIST is not None) and (
+                pcb_material_manufacturer not in self.LAMINATE_MATERIAL_MANUFACTURER_LIST
+            ):
+                raise SherlockGenStackupError(
+                    message="Invalid laminate manufacturer, grade, or material provided"
+                )
+            self._check_pcb_material_validity(
+                pcb_material_manufacturer, pcb_material_grade, pcb_material
+            )
+            if conductor_layers_cnt <= 1:
+                raise SherlockGenStackupError(
+                    message="The number of conductor layers must be greater than 1"
+                )
+            elif signal_layer_thickness < 0:
+                raise SherlockGenStackupError(message="Invalid conductor thickness provided")
+            elif board_thickness > 0:
+                if (
+                    (board_thickness_unit != "oz")
+                    and (self.LAMINATE_THICKNESS_UNIT_LIST is not None)
+                    and (board_thickness_unit not in self.LAMINATE_THICKNESS_UNIT_LIST)
+                ):
+                    raise SherlockGenStackupError(
+                        message="Invalid conductor thickness unit provided"
+                    )
+            elif min_laminate_thickness < 0:
+                raise SherlockGenStackupError(message="Invalid laminate thickness provided")
+            elif min_laminate_thickness > 0:
+                if (self.LAMINATE_THICKNESS_UNIT_LIST is not None) and (
+                    min_laminate_thickness_unit not in self.LAMINATE_THICKNESS_UNIT_LIST
+                ):
+                    raise SherlockGenStackupError(
+                        message="Invalid laminate thickness unit provided"
+                    )
+        except SherlockGenStackupError as e:
+            LOG.error(str(e))
+            raise e
+
         request = SherlockStackupService_pb2.GenStackupRequest(
             project=project,
             ccaName=cca_name,
