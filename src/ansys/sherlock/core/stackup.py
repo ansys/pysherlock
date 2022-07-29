dbb236fa88690551e5fc0d4975cfe6856072e27a
Ricky Gao <ricky.gao@ansys.com>
2022-07-29 10:27:57 -0400

    Cleaned up docuementation

--- src/ansys/sherlock/core/stackup.py
+++ src/ansys/sherlock/core/stackup.py
@@ -304,18 +304,21 @@ class Stackup(GrpcStub):
             The CCA name.
         layer : str, required
             The layer ID associated with this conductor layer.
-        type : str, required
+        type : str, optional
             Layer type (i.e. SIGNAL, POWER, SUBSTRATE).
-        material : str, required
+        material : str, optional
             Name of this conductor material.
-        thickness : double, required
+        thickness : double, optional
             Conductor layer thickness.
-        thickness_unit : str, required
+        thickness_unit : str, optional
             Conductor layer thickness unit.
-        conductor_percent : str, required
+        conductor_percent : str, optional
             Conductor percentage.
-        resin_material : str, required
+        resin_material : str, optional
             Resin material.
+
+        Note
+        ----
         Default values used will cause no changes for the given property.
 
         Example
