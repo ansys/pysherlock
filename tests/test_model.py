e4b92bf7f0ddf85c159fb55933b374123bf429f2
Paul Walters <paul.walters@ansys.com>
2022-11-30 16:10:30 -0500

    Added coordinate_units to export_trace_reinforcement_model.

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,22 +4,23 @@ import unittest
 
 from ansys.sherlock.core.model import Model
 
+
 class TestModel(unittest.TestCase):
     def test_model_export_file_directory(self):
         channel_param = "127.0.0.1:9090"
         channel = grpc.insecure_channel(channel_param)
         instance = Model(channel)
 
-        tempDir = os.environ.get('TEMP','C:\\TEMP');
+        temp_dir = os.environ.get('TEMP', 'C:\\TEMP')
 
         try:
             instance.export_trace_reinforcement_model(
-                "Tutorial Project", "Main Board", tempDir + "\\export.wbjn"
+                "Tutorial Project", "Main Board", temp_dir + "\\export.wbjn"
             )
         except Exception as e:
             assert (
-                str(e) == "Sherlock model service error: Export file directory "
-                "(" + tempDir + ") does not exist"
+                str(e) == "Sherlock model service error: Export file directory"
+                " (" + temp_dir + ") does not exist"
             )
 
 
