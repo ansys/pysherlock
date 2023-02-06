04def0321f4656e36d9f623ed2471a969780dc26
Miu Lin Edson <miulin.edson@ansys.com>
2023-02-06 11:40:17 -0500

    Added APIs get_random_vibe_input_fields and update_random_vibe_props.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,7 +1,8 @@
 import grpc
+import unittest
 
 from ansys.sherlock.core.analysis import Analysis
-from ansys.sherlock.core.errors import SherlockRunAnalysisError
+from ansys.sherlock.core.errors import SherlockRunAnalysisError, SherlockUpdateRandomVibePropsError
 
 
 def test_all():
@@ -11,6 +12,8 @@ def test_all():
     analysis = Analysis(channel)
 
     helper_test_run_analysis(analysis)
+    helper_test_translate_random_vibe_field_names(analysis)
+    helper_test_update_random_vibe_props(analysis)
 
 
 def helper_test_run_analysis(analysis):
@@ -84,6 +87,74 @@ def helper_test_run_analysis(analysis):
             "Run analysis error: Invalid analysis 0:" " Invalid phase 0: Invalid event(s) name"
         )
 
+def helper_test_translate_random_vibe_field_names(analysis):
+    """Test translating the random vibe field names."""
+
+    results = analysis._translate_random_vibe_field_names([
+        "randomVibeDamping", "naturalFreqMin", "naturalFreqMinUnits",
+        "naturalFreqMax", "naturalFreqMaxUnits", "analysisTemp",
+        "analysisTempUnits", "partValidationEnabled", "forceModelRebuild",
+        "reuseModalAnalysis", "performNFFreqRangeCheck", "requireMaterialAssignmentEnabled"
+    ])
+
+    expected = """
+random_vibe_damping
+natural_freq_min
+natural_freq_min_units
+natural_freq_max
+natural_freq_max_units
+analysis_temp
+analysis_temp_units
+part_validation_enabled
+force_model_rebuild
+reuse_modal_analysis
+perform_nf_freq_range_check
+require_material_assignment_enabled"""
+
+    assert results == expected
+
+def helper_test_update_random_vibe_props(analysis):
+    try:
+        analysis.update_random_vibe_props("", "Card", random_vibe_damping="0.01, 0.05",
+                                          analysis_temp=20, analysis_temp_units="C")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid project name"
+
+    try:
+        analysis.update_random_vibe_props("Test", "", random_vibe_damping="0.01, 0.05",
+                                          analysis_temp=20, analysis_temp_units="C")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid cca name"
+
+    try:
+        analysis.update_random_vibe_props("Test", "Card", random_vibe_damping="0.01, foo")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid random vibe damping value: foo"
+
+    try:
+        analysis.update_random_vibe_props("Test", "Card", random_vibe_damping="0.01, 0.02",
+                                          natural_freq_min_units="foo", natural_freq_max_units="HZ")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid min natural freq unit specified: foo"
+
+    try:
+        analysis.update_random_vibe_props("Test", "Card", random_vibe_damping="0.01, 0.02",
+                                          natural_freq_min_units="HZ", natural_freq_max_units="foo")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid max natural freq unit specified: foo"
+
+    try:
+        analysis.update_random_vibe_props("Test", "Card", random_vibe_damping="0.01, 0.02",
+                                          natural_freq_min_units="HZ", natural_freq_max_units="HZ",
+                                          analysis_temp_units="foo")
+        assert False
+    except SherlockUpdateRandomVibePropsError as e:
+        assert str(e) == "Update random vibe properties error: Invalid analysis temperature unit specified: foo"
 
 if __name__ == "__main__":
     test_all()
