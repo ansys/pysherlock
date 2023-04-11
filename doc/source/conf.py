f31e2a9f50585c77abe95ef51b34feb45391a0f4
Miu Lin Edson <miulin.edson@ansys.com>
2023-04-11 09:08:41 -0400

    Updated pyansys-sphinx-theme to ansys-sphinx-theme.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@ html_theme = "pyansys_sphinx_theme"
 
 # specify the location of your github repo
 html_theme_options = {
-    "github_url": "https://github.com/pyansys/pyansys-sphinx-theme",
+    "github_url": "https://github.com/pyansys/ansys-sphinx-theme",
     "show_prev_next": False,
 }
 
