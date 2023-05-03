cb8f4875b499f9ff950716391e858f6466286b6f
Ansys-rgiannam <129109201+Ansys-rgiannam@users.noreply.github.com>
2023-05-03 18:03:24 -0400

    Create load_harmonic_profile method (#97)

--- tests/test_lifecycle.py
+++ tests/test_lifecycle.py
@@ -16,6 +16,7 @@ from ansys.sherlock.core.errors import (
     SherlockLoadRandomVibeProfileError,
     SherlockLoadThermalProfileError,
     SherlockLoadShockProfileDatasetError,
+    SherlockLoadHarmonicProfileError,
 )
 from ansys.sherlock.core.lifecycle import Lifecycle
 
@@ -39,6 +40,7 @@ def test_all():
     helper_test_load_thermal_profile(lifecycle)
     helper_test_load_harmonic_profile(lifecycle)
     helper_test_load_shock_profile_dataset(lifecycle)
+    helper_test_load_harmonic_profile(lifecycle)
 
 
 def helper_test_create_life_phase(lifecycle):
@@ -1955,6 +1957,34 @@ def helper_test_load_shock_profile_dataset(lifecycle):
         assert False
     except SherlockLoadShockProfileDatasetError as e:
         assert str(e) == "Load shock profile dataset error: File path is invalid."
+
+
+def helper_test_load_harmonic_profile(lifcycle):
+    """Test load_harmonic_profile API."""
+
+    try:
+        lifcycle.load_harmonic_profile("", "Phase 1", "Harmonic Event", "Test_Profile.dat")
+        assert False
+    except SherlockLoadHarmonicProfileError as e:
+        assert "Load Harmonic profile error: Project name is invalid."
+
+    try:
+        lifcycle.load_harmonic_profile("Test", "", "Harmonic Event", "Test_Profile.dat")
+        assert False
+    except SherlockLoadHarmonicProfileError as e:
+        assert "Load Harmonic profile error: Phase name is invalid."
+
+    try:
+        lifcycle.load_harmonic_profile("Test", "Phase 1", "", "Test_Profile.dat")
+        assert False
+    except SherlockLoadHarmonicProfileError as e:
+        assert "Load Harmonic profile error: Event name is invalid."
+
+    try:
+        lifcycle.load_harmonic_profile("Test", "Phase 1", "Harmonic Event", "")
+        assert False
+    except SherlockLoadHarmonicProfileError as e:
+        assert "Load Harmonic profile error: File name is invalid."
 
 
 if __name__ == "__main__":
