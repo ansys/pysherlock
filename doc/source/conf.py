49b97daafa83ddd702495f56e7cbcb789f0a680e
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2024-07-24 15:35:13 +0200

    fix: link check with docs build (#339)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -113,3 +113,5 @@ latex_additional_files = [watermark, ansys_logo_white, ansys_logo_white_cropped]
 # change the preamble of latex with customized title page
 # variables are the title of pdf, watermark
 latex_elements = {"preamble": latex.generate_preamble(html_title)}
+
+linkcheck_ignore = ["https://www.ansys.com/*"]
