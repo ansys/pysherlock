a17068bce10cfdd5207c991717352753ceb12731
Nayane Fernandes <143632290+ansnfernand@users.noreply.github.com>
2024-11-14 15:13:42 +0100

    Update documentation with sphinx-gallery

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,6 +13,7 @@ from ansys_sphinx_theme import (
     watermark,
 )
 from sphinx.builders.latex import LaTeXBuilder
+from sphinx_gallery.sorting import FileNameSortKey
 
 from ansys.sherlock.core import __version__
 
@@ -23,7 +24,7 @@ project = "ansys-sherlock-core"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
 author = "ANSYS Inc."
 release = version = __version__
-cname = os.getenv("DOCUMENTATION_CNAME", default="nocname.com")
+cname = os.getenv("DOCUMENTATION_CNAME", default="sherlock.docs.pyansys.com")
 switcher_version = get_version_match(__version__)
 
 # Select desired logo, theme, and declare the html title
@@ -40,6 +41,7 @@ html_context = {
     "doc_path": "doc/source",
 }
 html_theme_options = {
+    "logo": "pyansys",
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
         "version_match": switcher_version,
@@ -50,6 +52,7 @@ html_theme_options = {
     "show_breadcrumbs": True,
     "collapse_navigation": True,
     "use_edit_page_button": True,
+    "header_links_before_dropdown": 5,  # number of links before the dropdown menu
     "additional_breadcrumbs": [
         ("PyAnsys", "https://docs.pyansys.com/"),
     ],
@@ -64,11 +67,21 @@ html_theme_options = {
 
 # Sphinx extensions
 extensions = [
+    "jupyter_sphinx",
+    "notfound.extension",
+    "numpydoc",
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
     "sphinx_design",
+    "sphinx_gallery.gen_gallery",
+    "sphinxemoji.sphinxemoji",
 ]
 
 autodoc_default_flags = ["members"]
@@ -85,6 +98,9 @@ master_doc = "index"
 
 # static path
 html_static_path = ["_static"]
+templates_path = ["_templates"]
+# The suffix(es) of source filenames.
+source_suffix = ".rst"
 
 # We have our own custom templates
 templates_path = ["_templates"]
@@ -123,5 +139,65 @@ linkcheck_ignore = ["https://www.ansys.com/*"]
 if switcher_version != "dev":
     linkcheck_ignore.append(f"https://github.com/ansys/pysherlock/releases/tag/v{version}")
 
-# Suprpress warnings
-suppress_warnings = ["design.grid", "design.fa-build"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "links.rst",
+]
+
+# -- Sphinx Gallery Options ---------------------------------------------------
+sphinx_gallery_conf = {
+    # convert rst to md for ipynb
+    "pypandoc": True,
+    # path to your examples scripts
+    "examples_dirs": ["../../examples/"],
+    # path where to save gallery generated examples
+    "gallery_dirs": ["examples/gallery_examples"],
+    # Pattern to search for example files
+    "filename_pattern": r"\.py",
+    # Remove the "Download all examples" button from the top level gallery
+    "download_all_examples": False,
+    # Sort gallery example by file name instead of number of lines (default)
+    "within_subsection_order": FileNameSortKey,
+    # directory where function granular galleries are stored
+    "backreferences_dir": None,
+    # Modules for which function level galleries are created.  In
+    "doc_module": "ansys-sherlock-core",
+    "image_scrapers": ("matplotlib"),
+    "ignore_pattern": "flycheck*",
+    "thumbnail_size": (350, 350),
+}
+
+# make rst_epilog a variable, so you can add other epilog parts to it
+rst_epilog = ""
+# Read link all targets from file
+with open("links.rst") as f:
+    rst_epilog += f.read()
+rst_epilog = rst_epilog.replace("%%VERSION%%", f"v{release}")
+
+# Copy button customization ---------------------------------------------------
+# exclude traditional Python prompts from the copied code
+copybutton_prompt_text = r">>> ?|\.\.\. "
+copybutton_prompt_is_regexp = True
+
+# numpydoc configuration
+numpydoc_use_plots = True
+numpydoc_show_class_members = False
+numpydoc_xref_param_type = True
+numpydoc_validate = True
+numpydoc_validation_checks = {
+    # "GL06",  # Found unknown section
+    # "GL07",  # Sections are in the wrong order.
+    "GL08",  # The object does not have a docstring
+    "GL09",  # Deprecation warning should precede extended summary
+    "GL10",  # reST directives {directives} must be followed by two colons
+    "SS01",  # No summary found
+    "SS02",  # Summary does not start with a capital letter
+    # "SS03", # Summary does not end with a period
+    "SS04",  # Summary contains heading whitespaces
+    # "SS05", # Summary must start with infinitive verb, not third person
+    "RT02",  # The first line of the Returns section should contain only the
+    # type, unless multiple values are being returned"
+}
