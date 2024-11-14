a49245664d74e6eb7187fd0ece40a149978cdcb6
Jorge Martinez <jorge.martinezgarrido@ansys.com>
2024-11-14 16:04:02 +0100

    fix: sphinx design warnings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -124,3 +124,6 @@ if switcher_version != "dev":
     linkcheck_ignore.append(
         f"https://github.com/ansys/pysherlock/releases/tag/v{pysherlock.__version__}"
     )
+
+# Suprpress warnings
+suppress_warnings = ["design.grid", "design.fa-build"]
