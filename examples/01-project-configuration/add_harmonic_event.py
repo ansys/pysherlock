ab48b38665419207e79de07c1a8b4e52a5617a40
Nayane Fernandes <143632290+ansnfernand@users.noreply.github.com>
2025-01-02 14:40:12 +0100

    Fix code style

--- examples/01-project-configuration/add_harmonic_event.py
+++ examples/01-project-configuration/add_harmonic_event.py
@@ -23,12 +23,12 @@
 Add Harmonic Event to Lifecycle Phase
 =========================================
 
-This example demonstrates how to launch the Sherlock gRPC service, import an ODB++ archive, 
+This example demonstrates how to launch the Sherlock gRPC service, import an ODB++ archive,
 create a lifecycle phase, and add a harmonic event to the phase, then properly close the connection.
 
 Description
 -----------
-Sherlock's gRPC API allows users to automate workflows such as creating lifecycle phases 
+Sherlock's gRPC API allows users to automate workflows such as creating lifecycle phases
 and adding harmonic events. This script shows how to:
 
 - Launch the Sherlock service.
@@ -37,23 +37,25 @@ and adding harmonic events. This script shows how to:
 - Add a harmonic event to the lifecycle phase.
 - Properly close the gRPC connection.
 
-The harmonic event can be used for thermal analysis and helps in simulating the effects of various conditions on the board.
+The harmonic event can be used for thermal analysis and helps in simulating the effects
+of various conditions on the board.
 """
 
 # sphinx_gallery_thumbnail_path = './images/add_harmonic_event_example.png'
 
 import os
 import time
-from ansys.sherlock.core.errors import SherlockAddHarmonicEventError
+
 from ansys.sherlock.core import launcher
+from ansys.sherlock.core.errors import SherlockAddHarmonicEventError
 
 ###############################################################################
 # Launch PySherlock service
 # ==========================
 # Launch the Sherlock service and ensure proper initialization.
 
-VERSION = '252'
-ANSYS_ROOT = os.getenv('AWP_ROOT' + VERSION)
+VERSION = "252"
+ANSYS_ROOT = os.getenv("AWP_ROOT" + VERSION)
 
 sherlock = launcher.launch_sherlock(port=9092)
 
@@ -71,7 +73,7 @@ sherlock.project.import_odb_archive(
     include_layers=True,
     use_stackup=True,
     project="Test",
-    cca_name="Card"
+    cca_name="Card",
 )
 print("ODB++ archive imported successfully.")
 
@@ -86,7 +88,7 @@ sherlock.lifecycle.create_life_phase(
     time_duration=1.5,
     time_units="sec",
     cycle_count=4.0,
-    cycle_units="COUNT"
+    cycle_units="COUNT",
 )
 print("Lifecycle phase 'Example' created successfully.")
 
@@ -107,7 +109,7 @@ try:
         frequency=5,
         direction="45,45",
         load_type="Uniaxial",
-        components="2,4,5"
+        components="2,4,5",
     )
     print("Harmonic event 'Event1' added successfully.")
 except SherlockAddHarmonicEventError as e:
