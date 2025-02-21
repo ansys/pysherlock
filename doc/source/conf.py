3af7e9294b6c462f8d7bdbcdd41447ee16181b56
Jeff Moody <110494049+ansjmoody@users.noreply.github.com>
2025-02-21 10:11:28 -0600

    conf.py: changed logo in html_theme_options

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -58,7 +58,7 @@ html_context = {
     "doc_path": str(DOC_PATH),
 }
 html_theme_options = {
-    "logo": "pyansys",
+    "logo": "pyansys_logo_black",
     "switcher": {
         "json_url": f"https://{cname}/versions.json",
         "version_match": switcher_version,
