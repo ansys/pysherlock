82d5c1464b0943f95a1a7bac657d6fdd0d2b1682
Jeff Moody <110494049+ansjmoody@users.noreply.github.com>
2025-06-16 14:51:24 -0500

    Fix copyright format issues

--- src/ansys/sherlock/core/types/project_types.py
+++ src/ansys/sherlock/core/types/project_types.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024-2025 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2021 - 2025 ANSYS, Inc. and/or its affiliates.
 
 """Module containing types for the Project Service."""
 
