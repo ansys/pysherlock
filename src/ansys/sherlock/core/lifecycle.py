45f79fcaf77834a71eccb537821c33e326b052d7
ansys-pwalters <119450156+ansys-pwalters@users.noreply.github.com>
2025-11-18 10:35:16 -0500

    fix: Unable to load CSV files into life cycle profiles (#667)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/sherlock/core/lifecycle.py
+++ src/ansys/sherlock/core/lifecycle.py
@@ -42,11 +42,16 @@ from ansys.sherlock.core.grpc_stub import GrpcStub
 from ansys.sherlock.core.types.lifecycle_types import (
     DeleteEventRequest,
     DeletePhaseRequest,
+    HarmonicVibeProfileCsvFileProperties,
     ImportThermalSignalRequest,
+    RandomVibeProfileCsvFileProperties,
     SaveHarmonicProfileRequest,
     SaveRandomVibeProfileRequest,
     SaveShockPulseProfileRequest,
     SaveThermalProfileRequest,
+    ShockProfileDatasetCsvFileProperties,
+    ShockProfilePulsesCsvFileProperties,
+    ThermalProfileCsvFileProperties,
 )
 from ansys.sherlock.core.utils.version_check import require_version
 
@@ -1750,7 +1755,12 @@ class Lifecycle(GrpcStub):
 
     @require_version()
     def load_random_vibe_profile(
-        self, project: str, phase_name: str, event_name: str, file_path: str
+        self,
+        project: str,
+        phase_name: str,
+        event_name: str,
+        file_path: str,
+        csv_file_properties: RandomVibeProfileCsvFileProperties = None,
     ) -> int:
         """Load random vibe profile from .csv or .dat file.
 
@@ -1765,7 +1775,9 @@ class Lifecycle(GrpcStub):
         event_name: str
             Name of the random vibe event.
         file_path: str
-            File path for thermal profile .dat or .csv file
+            File path for thermal profile .csv or .dat file
+        csv_file_properties: RandomVibeProfileCsvFileProperties
+            Properties of the random vibe profile CSV file, required if the file is in CSV format.
 
         Returns
         -------
@@ -1782,15 +1794,25 @@ class Lifecycle(GrpcStub):
             True,
             True,
             True,
-            project="Test",
+            project="Test Project",
             cca_name="Card"
         )
 
         >>> sherlock.lifecycle.load_random_vibe_profile(
-                project="Tutorial",
+                project="Test Project",
                 phase_name="Phase 1",
                 event_name="Random Event",
-                file_path="TestProfile.dat"
+                file_path="TestProfile.csv",
+                csv_file_properties=RandomVibeProfileCsvFileProperties(
+                    profile_name="Test Profile",
+                    header_row_count=0,
+                    numeric_format="English",
+                    column_delimiter=",",
+                    frequency_column="Frequency",
+                    frequency_units="HZ",
+                    amplitude_column="Amplitude",
+                    amplitude_units="G2/Hz"
+                )
         )
         """
         try:
@@ -1802,6 +1824,17 @@ class Lifecycle(GrpcStub):
                 raise SherlockLoadRandomVibeProfileError(message="Event name is invalid.")
             if file_path == "":
                 raise SherlockLoadRandomVibeProfileError(message="File path is invalid.")
+            if file_path.lower().endswith(".csv"):
+                if csv_file_properties is None:
+                    raise SherlockLoadRandomVibeProfileError(
+                        "CSV file properties must be provided for CSV random vibe profile files."
+                    )
+            else:
+                if csv_file_properties is not None:
+                    raise SherlockLoadRandomVibeProfileError(
+                        "CSV file properties are not used for non-CSV random vibe profile files."
+                    )
+
             if not self._is_connection_up():
                 raise SherlockNoGrpcConnectionException()
 
@@ -1810,6 +1843,9 @@ class Lifecycle(GrpcStub):
                 phaseName=phase_name,
                 eventName=event_name,
                 filePath=file_path,
+                randomVibeCsvProps=(
+                    csv_file_properties._convert_to_grpc() if csv_file_properties else None
+                ),
             )
             response = self.stub.loadRandomVibeProfile(request)
             return_code = response.returnCode
@@ -1824,9 +1860,14 @@ class Lifecycle(GrpcStub):
 
     @require_version()
     def load_thermal_profile(
-        self, project: str, phase_name: str, event_name: str, file_path: str
+        self,
+        project: str,
+        phase_name: str,
+        event_name: str,
+        file_path: str,
+        csv_file_properties: ThermalProfileCsvFileProperties = None,
     ) -> int:
-        """Load a thermal profile from a .dat or .csv file.
+        """Load a thermal profile from a .csv or .dat file.
 
         Available Since: 2021R1
 
@@ -1839,7 +1880,9 @@ class Lifecycle(GrpcStub):
         event_name: str
             Name of the random vibe event.
         file_path: str
-            File path for thermal profile .dat or .csv file
+            File path for thermal profile .csv or .dat file
+        csv_file_properties: ThermalProfileCsvFileProperties
+            Properties of the thermal profile CSV file, required if the file is in CSV format.
 
         Returns
         -------
@@ -1856,14 +1899,26 @@ class Lifecycle(GrpcStub):
             True,
             True,
             True,
-            project="Test",
+            project="Test Project",
             cca_name="Card",
         )
-         >>>loaded = sherlock.lifecycle.load_thermal_profile(
-                project="Tutorial",
+         >>> sherlock.lifecycle.load_thermal_profile(
+                project="Test Project",
                 phase_name="Phase 1",
                 event_name="Thermal Event",
-                file_path="Tutorial_Profile.dat"
+                file_path="Tutorial_Profile.csv",
+                csv_file_properties=ThermalProfileCsvFileProperties(
+                    profile_name="Test Profile",
+                    header_row_count=0,
+                    numeric_format="English",
+                    column_delimiter=",",
+                    step_column="Step",
+                    type_column="Type",
+                    time_column="Time (min)",
+                    time_units="min",
+                    temp_column="Temp (C)",
+                    temp_units="C"
+                )
         )
         """
         try:
@@ -1875,6 +1930,17 @@ class Lifecycle(GrpcStub):
                 raise SherlockLoadThermalProfileError(message="Event name is invalid.")
             if file_path == "":
                 raise SherlockLoadThermalProfileError(message="File path is invalid.")
+            if file_path.lower().endswith(".csv"):
+                if csv_file_properties is None:
+                    raise SherlockLoadThermalProfileError(
+                        "CSV file properties must be provided for CSV thermal profile files."
+                    )
+            else:
+                if csv_file_properties is not None:
+                    raise SherlockLoadThermalProfileError(
+                        "CSV file properties are not used for non-CSV thermal profile files."
+                    )
+
             if not self._is_connection_up():
                 raise SherlockNoGrpcConnectionException()
 
@@ -1883,6 +1949,7 @@ class Lifecycle(GrpcStub):
                 phaseName=phase_name,
                 eventName=event_name,
                 filePath=file_path,
+                csvProps=csv_file_properties._convert_to_grpc() if csv_file_properties else None,
             )
             response = self.stub.loadThermalProfile(request)
             return_code = response.returnCode
@@ -1902,9 +1969,15 @@ class Lifecycle(GrpcStub):
 
     @require_version()
     def load_harmonic_profile(
-        self, project: str, phase_name: str, event_name: str, file_path: str
+        self,
+        project: str,
+        phase_name: str,
+        event_name: str,
+        file_path: str,
+        triaxial_axis: str,
+        csv_file_properties: HarmonicVibeProfileCsvFileProperties = None,
     ) -> int:
-        """Load a harmonic profile from a DAT or CSV file to a life cycle phase.
+        """Load a harmonic profile from a .csv or .dat file to a life cycle phase.
 
         Available Since: 2021R1
 
@@ -1917,7 +1990,13 @@ class Lifecycle(GrpcStub):
         event_name: str
             Name of the harmonic event.
         file_path: str
-            Path for DAT or CSV file with the harmonic profile.
+            Path for .csv or .dat file with the harmonic profile.
+        triaxial_axis: str
+            Axis that this profile should be assigned to if the harmonic
+            profile type is ``"Triaxial"``. Options are: ``"x"``, ``"y"``,
+            and ``"z"``.
+        csv_file_properties: HarmonicProfileCsvFileProperties
+            Properties of the harmonic profile CSV file, required if the file is in CSV format.
 
         Returns
         -------
@@ -1934,15 +2013,26 @@ class Lifecycle(GrpcStub):
             True,
             True,
             True,
-            project="Test",
+            project="Test Project",
             cca_name="Card"
         )
 
-        >>> loaded = sherlock.lifecycle.load_harmonic_profile(
-                project="Tutorial",
+        >>> sherlock.lifecycle.load_harmonic_profile(
+                project="Test Project",
                 phase_name="Phase 1",
                 event_name="Harmonic Event",
-                file_path="Test_Profile.dat"
+                file_path="Test_Profile.csv",
+                triaxial_axis="x",
+                csv_file_properties=HarmonicVibeProfileCsvFileProperties(
+                    profile_name="Test Profile",
+                    header_row_count=0,
+                    numeric_format="English",
+                    column_delimiter=",",
+                    frequency_column="Frequency",
+                    frequency_units="HZ",
+                    load_column="Load",
+                    load_units="G"
+                )
         )
         """
         try:
@@ -1954,6 +2044,17 @@ class Lifecycle(GrpcStub):
                 raise SherlockLoadHarmonicProfileError(message="Event name is invalid.")
             if file_path == "":
                 raise SherlockLoadHarmonicProfileError(message="File name is invalid.")
+            if file_path.lower().endswith(".csv"):
+                if csv_file_properties is None:
+                    raise SherlockLoadHarmonicProfileError(
+                        "CSV file properties must be provided for CSV harmonic profile files."
+                    )
+            else:
+                if csv_file_properties is not None:
+                    raise SherlockLoadHarmonicProfileError(
+                        "CSV file properties are not used for non-CSV harmonic profile files."
+                    )
+
             if not self._is_connection_up():
                 raise SherlockNoGrpcConnectionException()
 
@@ -1962,6 +2063,10 @@ class Lifecycle(GrpcStub):
                 phaseName=phase_name,
                 eventName=event_name,
                 filePath=file_path,
+                harmonicCsvProps=(
+                    csv_file_properties._convert_to_grpc() if csv_file_properties else None
+                ),
+                triaxialAxis=triaxial_axis,
             )
             response = self.stub.loadHarmonicProfile(request)
             return_code = response.returnCode
@@ -1980,7 +2085,12 @@ class Lifecycle(GrpcStub):
 
     @require_version()
     def load_shock_profile_dataset(
-        self, project: str, phase_name: str, event_name: str, file_path: str
+        self,
+        project: str,
+        phase_name: str,
+        event_name: str,
+        file_path: str,
+        csv_file_properties: ShockProfileDatasetCsvFileProperties = None,
     ) -> int:
         """Load shock profile dataset from a .csv or .dat file.
 
@@ -1995,7 +2105,9 @@ class Lifecycle(GrpcStub):
         event_name: str
             Name of the random vibe event.
         file_path: str
-            File path for thermal profile .dat or .csv file
+            File path for thermal profile .csv or .dat file
+        csv_file_properties: ShockProfileDatasetCsvFileProperties
+            Properties of the shock profile dataset CSV file, required if the file is in CSV format.
 
         Returns
         -------
@@ -2012,10 +2124,26 @@ class Lifecycle(GrpcStub):
             True,
             True,
             True,
-            project="Test",
+            project="Test Project",
             cca_name="Card"
         )
 
+        >>> sherlock.lifecycle.load_shock_profile_dataset(
+                project="Test Project",
+                phase_name="Phase 1",
+                event_name="Shock Event",
+                file_path="Test_Profile.csv",
+                csv_file_properties=ShockProfileDatasetCsvFileProperties(
+                    profile_name="Test Profile",
+                    header_row_count=0,
+                    numeric_format="English",
+                    column_delimiter=",",
+                    time_column="Time",
+                    time_units="ms",
+                    load_column="Load",
+                    load_units="G"
+                )
+        )
         """
         try:
             if project == "":
@@ -2026,14 +2154,28 @@ class Lifecycle(GrpcStub):
                 raise SherlockLoadShockProfileDatasetError(message="Event name is invalid.")
             if file_path == "":
                 raise SherlockLoadShockProfileDatasetError(message="File path is invalid.")
+            if file_path.lower().endswith(".csv"):
+                if csv_file_properties is None:
+                    raise SherlockLoadShockProfileDatasetError(
+                        "CSV file properties must be provided for CSV shock profile dataset files."
+                    )
+            else:
+                if csv_file_properties is not None:
+                    raise SherlockLoadShockProfileDatasetError(
+                        "CSV file properties are not used for non-CSV shock profile dataset files."
+                    )
+
             if not self._is_connection_up():
                 raise SherlockNoGrpcConnectionException()
 
-            request = SherlockLifeCycleService_pb2.LoadShockProfilePulsesRequest(
+            request = SherlockLifeCycleService_pb2.LoadShockProfileDatasetRequest(
                 project=project,
                 phaseName=phase_name,
                 eventName=event_name,
                 filePath=file_path,
+                shockDsCsvProps=(
+                    csv_file_properties._convert_to_grpc() if csv_file_properties else None
+                ),
             )
             response = self.stub.loadShockProfileDataset(request)
             return_code = response.returnCode
@@ -2050,7 +2192,12 @@ class Lifecycle(GrpcStub):
 
     @require_version()
     def load_shock_profile_pulses(
-        self, project: str, phase_name: str, event_name: str, file_path: str
+        self,
+        project: str,
+        phase_name: str,
+        event_name: str,
+        file_path: str,
+        csv_file_properties: ShockProfilePulsesCsvFileProperties = None,
     ) -> int:
         """Load shock profile pulses from a .csv .dat file.
 
@@ -2065,7 +2212,9 @@ class Lifecycle(GrpcStub):
         event_name: str
             Name of the random vibe event.
         file_path: str
-            Path for thermal profile .dat or .csv file
+            Path for thermal profile .csv or .dat file
+        csv_file_properties: ShockProfilePulsesCsvFileProperties
+            Properties of the shock profile pulses CSV file, required if the file is in CSV format.
 
         Returns
         -------
@@ -2089,9 +2238,24 @@ class Lifecycle(GrpcStub):
                 project="Tutorial",
                 phase_name="Phase 1",
                 event_name="Shock Event",
-                file_path="Test_Profile.dat"
+                file_path="Test_Profile.csv",
+                csv_file_properties=ShockProfilePulsesCsvFileProperties(
+                    profile_name="Test Profile",
+                    header_row_count=0,
+                    numeric_format="English",
+                    column_delimiter=",",
+                    duration=25,
+                    duration_units="ms",
+                    sample_rate=0.1,
+                    sample_rate_units="ms",
+                    shape_column="Shape",
+                    load_column="Load",
+                    load_units="G",
+                    frequency_column="Frequency",
+                    frequency_units="HZ",
+                    decay_column="Decay",
+                )
         )
-
         """
         try:
             if project == "":
@@ -2102,6 +2266,17 @@ class Lifecycle(GrpcStub):
                 raise SherlockLoadShockProfilePulsesError(message="Event name is invalid.")
             if file_path == "":
                 raise SherlockLoadShockProfilePulsesError(message="File path is invalid.")
+            if file_path.lower().endswith(".csv"):
+                if csv_file_properties is None:
+                    raise SherlockLoadShockProfilePulsesError(
+                        "CSV file properties must be provided for CSV shock profile pulses files."
+                    )
+            else:
+                if csv_file_properties is not None:
+                    raise SherlockLoadShockProfilePulsesError(
+                        "CSV file properties are not used for non-CSV shock profile pulses files."
+                    )
+
             if not self._is_connection_up():
                 raise SherlockNoGrpcConnectionException()
 
@@ -2110,6 +2285,9 @@ class Lifecycle(GrpcStub):
                 phaseName=phase_name,
                 eventName=event_name,
                 filePath=file_path,
+                shockPulsesCsvProps=(
+                    csv_file_properties._convert_to_grpc() if csv_file_properties else None
+                ),
             )
             response = self.stub.loadShockProfilePulses(request)
             return_code = response.returnCode
@@ -2186,7 +2364,7 @@ class Lifecycle(GrpcStub):
     def save_harmonic_profile(
         self, request: SaveHarmonicProfileRequest
     ) -> SherlockCommonService_pb2.ReturnCode:
-        """Save a harmonic life cycle event profile to a .dat or .csv file.
+        """Save a harmonic life cycle event profile to a .csv or .dat file.
 
         Available Since: 2026R1
 
@@ -2233,7 +2411,7 @@ class Lifecycle(GrpcStub):
     def save_random_vibe_profile(
         self, request: SaveRandomVibeProfileRequest
     ) -> SherlockCommonService_pb2.ReturnCode:
-        """Save a random vibe life cycle event profile to a .dat or .csv file.
+        """Save a random vibe life cycle event profile to a .csv or .dat file.
 
         Available Since: 2026R1
 
@@ -2277,7 +2455,7 @@ class Lifecycle(GrpcStub):
     def save_shock_pulse_profile(
         self, request: SaveShockPulseProfileRequest
     ) -> SherlockCommonService_pb2.ReturnCode:
-        """Save a shock pulse life cycle event profile to a .dat or .csv file.
+        """Save a shock pulse life cycle event profile to a .csv or .dat file.
 
         Available Since: 2026R1
 
@@ -2321,7 +2499,7 @@ class Lifecycle(GrpcStub):
     def save_thermal_profile(
         self, request: SaveThermalProfileRequest
     ) -> SherlockCommonService_pb2.ReturnCode:
-        """Save a thermal life cycle event profile to a .dat or .csv file.
+        """Save a thermal life cycle event profile to a .csv or .dat file.
 
         Available Since: 2026R1
 
