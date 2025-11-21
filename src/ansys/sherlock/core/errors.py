601e0a49fb42beedd867a0f01a0c3e363b54387a
Nayane Fernandes <143632290+ansnfernand@users.noreply.github.com>
2025-11-21 15:21:01 +0100

    fix: fixed unit tests

--- src/ansys/sherlock/core/errors.py
+++ src/ansys/sherlock/core/errors.py
@@ -224,13 +224,17 @@ class SherlockCreateLifePhaseError(Exception):
 class SherlockAddRandomVibeEventError(Exception):
     """Contains the errors raised when a random vibe event cannot be added."""
 
-    def __init__(self, message: str):
+    def __init__(self, message: Optional[str] = None, error_array: Optional[list[str]] = None):
         """Initialize error message."""
         self.message = message
+        self.error_array = error_array
 
     def str_itr(self):
         """Create list of error messages."""
-        if self.message is not None:
+        if self.error_array is not None:
+            return [f"Add random vibe event error: {error}" for error in self.error_array]
+
+        elif self.message is not None:
             return [f"Add random vibe event error: {self.message}"]
 
 
