eeba4790b4de40410b37c0618d712dd8ffa72d69
skola-ansys <santosh.kola@ansys.com>
2026-01-06 17:45:52 -0500

    feat: Optimization version check (#658)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>
    Co-authored-by: Jeff Moody <110494049+ansjmoody@users.noreply.github.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 - 2025 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2021 - 2026 ANSYS, Inc. and/or its affiliates.
 # © 2023 ANSYS, Inc. All rights reserved.
 
 """PySherlock unit tests."""
