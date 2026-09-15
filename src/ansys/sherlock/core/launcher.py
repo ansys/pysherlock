79059c87b75410a85614f06582a38a3e5208ecc2
Keith Hanson <khanson@synopsys.com>
2026-09-15 12:30:04 -0400

    refactor: removed try catch so that connection error will be raised instead of generic error

--- src/ansys/sherlock/core/launcher.py
+++ src/ansys/sherlock/core/launcher.py
@@ -239,18 +239,16 @@ def launch_and_connect(
         uds_dir=uds_dir,
         uds_id=uds_id,
     )
-    try:
-        sherlock = connect(
-            port=port,
-            timeout=timeout,
-            transport_mode=transport_mode,
-            uds_dir=uds_dir,
-            uds_id=uds_id,
-        )
-        return sherlock, ansys_install_path
-    except Exception as e:
-        LOG.error(f"Error connecting to Sherlock after launch: {e}")
-        raise RuntimeError(f"Error connecting to Sherlock after launch: {e}")
+
+    sherlock = connect(
+        port=port,
+        timeout=timeout,
+        transport_mode=transport_mode,
+        uds_dir=uds_dir,
+        uds_id=uds_id,
+    )
+    return sherlock, ansys_install_path
+
 
 
 def connect(
