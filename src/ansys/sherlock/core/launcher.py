cafe631fb76c2115725bd46dab2ee1ed3ba17fef
Keith Hanson <122566160+anskhanson@users.noreply.github.com>
2026-09-23 11:13:17 -0400

    feat: Added import odb single project (#965)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/sherlock/core/launcher.py
+++ src/ansys/sherlock/core/launcher.py
@@ -24,8 +24,11 @@
 
 """Module for launching Sherlock locally or connecting to a local instance with gRPC."""
 
+import collections
+from collections.abc import Callable
 import errno
 import os
+import re
 import shlex
 import socket
 from typing import Optional
@@ -392,15 +395,31 @@ def _wait_for_sherlock_grpc_ready(channel, timeout):
         raise SherlockConnectionError(message="Error starting gRPC service")
 
 
+def _extract_awp_root_vars_from_list(
+    env_var_dict: dict[str, str], check_for_sherlock_func: Callable[[str], bool]
+) -> dict[str, str]:
+    env_var_pattern = re.compile(r"AWP_ROOT\d\d\d")
+    supported_installed_versions = {
+        env_key: path
+        for env_key, path in env_var_dict.items()
+        if re.fullmatch(env_var_pattern, env_key) is not None and check_for_sherlock_func(path)
+    }
+    return collections.OrderedDict(
+        sorted(supported_installed_versions.items(), key=lambda item: item[0], reverse=True)
+    )
+
+
+def _is_sherlock_in_path(path: str) -> bool:
+    return os.path.isfile(_get_sherlock_exe_file_path(path))
+
+
 def _get_base_ansys(
     year: Optional[int] = None, release_number: Optional[int] = None
 ) -> tuple[str, int]:
-    supported_installed_versions = {
-        env_key: path
-        for env_key, path in os.environ.items()
-        if env_key.startswith("AWP_ROOT") and os.path.isfile(_get_sherlock_exe_file_path(path))
-    }
 
+    supported_installed_versions = _extract_awp_root_vars_from_list(
+        os.environ, _is_sherlock_in_path
+    )
     sorted_installed_version_keys = sorted(supported_installed_versions, reverse=True)
 
     if year is not None:
